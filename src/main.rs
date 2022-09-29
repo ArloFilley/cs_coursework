dd91be366b3a3277b1b20d50deeb48ee5e9584d4
ArloFilley <ArloFilley@gmail.com>
2022-09-29 10:24:20 +0100

    added functionality and file structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #[macro_use] extern crate rocket;
 use rocket::{Rocket, Build};
 
+#[get("/")]
+fn index() -> String {
+    format!("Hello world")
+}
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
+    .mount("/", routes![index])
 }
