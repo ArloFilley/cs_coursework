eb9cf983553cbfcecb512d453e2a7a5609160aa0
ArloFilley <ArloFilley@gmail.com>
2022-10-20 01:59:03 +0100

    i'm so tired please help

--- openbrowsers.sh
+++ openbrowsers.sh
@@ -1,3 +1,2 @@
-curl 'http://172.25.4.64:8000/test'
-chromium-browser http://172.25.4.64:8000
-chromium-browser http://172.25.3.64:8000/leaderboard
+chromium-browser http://127.0.0.1:8000
+chromium-browser http://127.0.0.1/leaderboard
--- testserver.sh
+++ testserver.sh
@@ -1,4 +1,4 @@
-sleep 10
+sleep 20
 curl "https://127.0.0.1:8000/test"
 sleep 1
 curl "https://127.0.0.1:8000/test"
