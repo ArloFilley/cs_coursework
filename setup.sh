e4f81cc4fde25032ad1cd8c8f88f9e8b41bc782b
ArloFilley <ArloFilley@gmail.com>
2022-10-20 02:12:15 +0100

    fixed typo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 yes | apt install chromium
 mkdir ./database && touch ./database/database.sqlite
-chmod 777 ./openbrowsers.sh ./startserver.sh ./testsever.sh
+chmod 777 ./openbrowsers.sh ./startserver.sh ./testserver.sh
 exit
