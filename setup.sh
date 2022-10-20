5035b288d7c4392806e14de9b798323e3f4b5d37
ArloFilley <ArloFilley@gmail.com>
2022-10-20 02:10:44 +0100

    hi howre you :)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 yes | apt install chromium
 mkdir ./database && touch ./database/database.sqlite
+chmod 777 ./openbrowsers.sh ./startserver.sh ./testsever.sh
 exit
