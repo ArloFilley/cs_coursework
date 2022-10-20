43a09e558053e9d598db013c2ab54f2d23386fc1
ArloFilley <ArloFilley@gmail.com>
2022-10-20 02:23:35 +0100

    i am so out of ideas for funnies at this point

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 yes | apt install chromium curl
-yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+yes 1 | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
 mkdir ./database && touch ./database/database.sqlite
 chmod 777 ./openbrowsers.sh ./startserver.sh ./testserver.sh
+./startserver.sh &
+./testserver.sh &
+./openbrowsers.sh &
 exit
