517231ca309ff6e12d9d34e506004791fa504c69
ArloFilley <ArloFilley@gmail.com>
2022-10-20 01:52:12 +0100

    changed to work on raspberry pi's

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ chmod 777 ./startserver.sh
 chmod 777 ./open_browsers.sh
 mkdir ./database
 touch ./database/database.sqlite
-gnome-terminal -x ./startserver.sh
-gnome-terminal -x ./open_browsers.sh
+xterm -e ./startserver.sh
+xterm -e ./open_browsers.sh
 exit
