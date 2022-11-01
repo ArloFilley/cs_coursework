c9e8fad0297f2870d60d8597855c92db00fa77cf
ArloFilley <ArloFilley@gmail.com>
2022-11-01 22:43:04 +0000

    database changes to support user authentication

--- src/sql.rs
+++ src/sql.rs
@@ -1,12 +1,19 @@
 use rusqlite::{Connection, Result};
 
-fn get_connection() -> rusqlite::Connection {
-    Connection::open("database/database.sqlite")
-        .expect("Error creating database connection")
+fn get_connection() -> Result<rusqlite::Connection, rusqlite::Connection> {
+    let conn = Connection::open("database/database.sqlite")
+        .expect("error getting database connection");
+    Ok(conn)
 }
 
-pub fn create_database() -> Result<()> {
-    let connection = get_connection();
+pub fn create_database() -> Result<(), rusqlite::Error> {
+    let connection = get_connection().expect("error getting database connection");
+
+    connection.execute("CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        user_name TEXT UNIQUE,
+        user_password TEXT NOT NULL)"
+    , ()).expect("error");
     
     connection.execute(
         "CREATE TABLE IF NOT EXISTS tests (
@@ -18,17 +25,18 @@ pub fn create_database() -> Result<()> {
             quote_id INTEGER,
             wpm INTEGER,
             accuracy INTEGER,
-            user_id INTEGER
+            user_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES users(user_id)
         )",
-        (), // empty parameters list
-    )?;
+        (),
+    ).expect("error creating tables");
 
     Ok(())
 }
 
 pub fn post_test(test_type: &str, test_length: i64, test_time: i32, test_seed: i64, quote_id: i32, wpm: i16, accuracy: i8, user_id: i32) 
--> Result<()> {
-    let connection = get_connection();
+-> Result<(), rusqlite::Error> {
+    let connection = get_connection().expect("error");
 
     connection.execute(
         "INSERT INTO tests (
